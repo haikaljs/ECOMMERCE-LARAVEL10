901f9f89dd6e64e563c0c19a3df0e11b7668ec67
haikaljs <haikaljavascript@gmail.com>
2024-02-12 02:23:03 +0800

    Frondend setup - Forget password page

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -35,10 +35,14 @@ class PasswordResetLinkController extends Controller
         $status = Password::sendResetLink(
             $request->only('email')
         );
-
-        return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                            ->withErrors(['email' => __($status)]);
+        
+        if($status == Password::RESET_LINK_SENT){
+            toastr('Reset link has been sent to your mail');
+            return back()->with('status', __($status));
+           
+        }else{
+            return back()->withInput($request->only('email'))
+                   ->withErrors(['email' => __($status)]);
+        }
     }
 }
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <form method="POST" action="{{ route('password.store') }}">
         @csrf
 
@@ -36,4 +36,58 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+@extends('frontend.layouts.master')
+
+@section('content')
+<section id="wsus__breadcrumb">
+    <div class="wsus_breadcrumb_overlay">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h4>Reset password</h4>
+                    <ul>
+                        <li><a href="#">login</a></li>
+                        <li><a href="#">reset password</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="wsus__login_register">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-5 col-md-10 col-lg-7 m-auto">
+                <form method="POST" action="{{ route('password.store') }}">
+                    @csrf
+                    <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                    <div class="wsus__change_password">
+                        <h4>reset password</h4>
+                        <div class="wsus__single_pass">
+                            <label>Email</label>
+                            <input type="email" name="email" value="{{old('email', $request->email)}}"  placeholder="Email">
+                        </div>
+                        <div class="wsus__single_pass">
+                            <label>New Password</label>
+                            <input type="password" name="password" placeholder="Password">
+                        </div>
+                        <div class="wsus__single_pass">
+                            <label>Confirm password</label>
+                            <input type="password" name="password_confirmation" placeholder="Confirm Password">
+                        </div>
+                        <button class="common_btn" type="submit">submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
+
+
+  
+
+
