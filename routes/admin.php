0ccb43e5a7546a57f205bccee1d933eed3db58f9
haikaljs <haikaljavascript@gmail.com>
2024-02-11 13:35:08 +0800

    Admin profile update part1

--- routes/admin.php
+++ routes/admin.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\ProfileController;
     
 // Admin routes
 Route::get('dashboard', [AdminController::class, 'dashboard'])->name('dashboard');
+
+// Profile routes
+Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+Route::post('profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
+
 ?>
