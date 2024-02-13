70247cfb95e208f7db9bef20fb717451e29ac858
haikaljs <haikaljavascript@gmail.com>
2024-02-14 00:11:18 +0800

    Slider - Update slider data

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -63,7 +63,7 @@ class SliderController extends Controller
      */
     public function show(string $id)
     {
-        //
+        
     }
 
     /**
@@ -71,7 +71,8 @@ class SliderController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $slider = Slider::findOrFail($id);
+        return view('admin.slider.edit', compact('slider'));
     }
 
     /**
@@ -79,7 +80,31 @@ class SliderController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'banner' => ['nullable', 'image', 'max:2000'],
+            'type' => ['max:200'],
+            'title' => ['required', 'max:200'],
+            'starting_price' => ['max:200'],
+            'btn_url' => ['url'],
+            'serial' => ['required', 'integer'],
+            'status' => ['required']
+        ]);
+
+        $slider = Slider::findOrFail($id);
+        // handle file upload
+        $imagePath = $this->updateImage($request, 'banner', 'uploads', $slider->banner);
+
+        $slider->banner = $imagePath;
+        $slider->type = $request->type;
+        $slider->title = $request->title;
+        $slider->starting_price = $request->starting_price;
+        $slider->btn_url = $request->btn_url;
+        $slider->serial = $request->serial;
+        $slider->status = $request->status;
+        $slider->save();
+
+        toastr('Updated successfully', 'success');
+        return redirect()->route('admin.slider.index');
     }
 
     /**
--- app/Traits/ImageUploadTrait.php
+++ app/Traits/ImageUploadTrait.php
@@ -2,6 +2,7 @@
 namespace App\Traits;
 
 use Illuminate\Http\Request;
+use File;
 
 trait ImageUploadTrait{
 
@@ -18,6 +19,27 @@ trait ImageUploadTrait{
          
            }
     }
+
+    public function updateImage(Request $request, $inputName, $path, $oldPath = null)
+    {
+        if($request->hasFile($inputName)){
+            if(File::exists(public_path($oldPath))){
+                File::delete(public_path($oldPath));
+            }
+            $image = $request->{$inputName};
+            $ext = $image->getClientOriginalExtension();
+            $imageName = 'media_'.uniqid().'.'.$ext;
+            $image->move(public_path($path), $imageName);
+    
+            return $path.'/'.$imageName;
+         
+           }
+    }
+
+
+    
+
+    
 }
 
 ?>
