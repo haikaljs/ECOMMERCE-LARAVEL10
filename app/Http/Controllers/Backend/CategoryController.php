eee4349a622fcc8d998e94642d2ff811a4847270
haikaljs <haikaljavascript@gmail.com>
2024-02-14 12:25:45 +0800

    Category - Update category

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -64,7 +64,8 @@ class CategoryController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $category = Category::findOrFail($id);
+        return view('admin.category.edit', compact('category'));
     }
 
     /**
@@ -72,7 +73,23 @@ class CategoryController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'icon' => ['required', 'not_in:empty'],
+            'name' => ['required', 'max:200', 'unique:categories,name,'.$id],
+            'status' => ['required']
+        ]);
+
+        $category = Category::findOrFail($id);
+
+        $category->icon = $request->icon;
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        $category->status = $request->status;
+        $category->save();
+
+        toastr('Updated successfully', 'success');
+        return redirect()->route('admin.category.index');
+
     }
 
     /**
