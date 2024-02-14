9eee22cc67724fd4233752bf8abc208114213dcc
haikaljs <haikaljavascript@gmail.com>
2024-02-14 12:38:40 +0800

    Category - Delete category

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -97,6 +97,11 @@ class CategoryController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $category = Category::findOrFail($id);
+        $category->delete();
+        return response([
+            'status' => 'success',
+            'message' => 'Deleted Successfully!'
+        ]);
     }
 }
