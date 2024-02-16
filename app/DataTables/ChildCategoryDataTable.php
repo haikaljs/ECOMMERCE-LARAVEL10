0db9bc43ada4067e63c34041c7f1d05975575e1f
haikaljs <haikaljavascript@gmail.com>
2024-02-16 20:14:11 +0800

    Child Category - Show created data at datatables

--- app/DataTables/ChildCategoryDataTable.php
+++ app/DataTables/ChildCategoryDataTable.php
@@ -22,7 +22,37 @@ class ChildCategoryDataTable extends DataTable
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
-            ->addColumn('action', 'childcategory.action')
+        ->addColumn('action', function($query){
+            $editBtn = "<a href='".route('admin.child-category.edit', $query->id)."' class='btn btn-primary'><i class='far fa-edit'></i></a>";
+
+            $deleteBtn = "<a href='".route('admin.child-category.destroy', $query->id)."'class='btn btn-danger ml-2 delete-item'><i class='far fa-trash-alt'></i></a>";
+
+            return $editBtn.$deleteBtn;
+        })
+            ->addColumn('category', function($query){
+                return $query->category->name;
+            })
+            ->addColumn('sub_category', function($query){
+                return $query->subCategory->name;
+            })
+            ->addColumn('status', function($query){
+                if($query->status == 1){
+                    $button = '<label class="custom-switch mt-2">
+                    <input type="checkbox" data-id="'.$query->id.'" checked name="custom-switch-checkbox" class="custom-switch-input change-status">
+                    <span class="custom-switch-indicator"></span>
+                   
+                  </label>';
+                }else{
+                    $button = '<label class="custom-switch mt-2">
+                    <input type="checkbox" data-id="'.$query->id.'" name="custom-switch-checkbox" class="custom-switch-input change-status">
+                    <span class="custom-switch-indicator"></span>
+                   
+                  </label>';
+                }
+            
+                return $button;
+            })
+            ->rawColumns(['action', 'status'])
             ->setRowId('id');
     }
 
@@ -62,15 +92,17 @@ class ChildCategoryDataTable extends DataTable
     public function getColumns(): array
     {
         return [
+            Column::make('id'),
+            Column::make('name'),
+            Column::make('category'),
+            Column::make('sub_category'),
+            Column::make('status'),
             Column::computed('action')
                   ->exportable(false)
                   ->printable(false)
-                  ->width(60)
+                  ->width(200)
                   ->addClass('text-center'),
-            Column::make('id'),
-            Column::make('add your columns'),
-            Column::make('created_at'),
-            Column::make('updated_at'),
+          
         ];
     }
 
