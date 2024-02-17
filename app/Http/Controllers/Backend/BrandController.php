95ba1dc5df26a24e2eb5519c80511e95a9a0e0ee
haikaljs <haikaljavascript@gmail.com>
2024-02-17 12:45:39 +0800

    Brand - Working with create feature

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers\Backend;
 
+use App\Models\Brand;
 use Illuminate\Http\Request;
+use App\Traits\ImageUploadTrait;
 use App\DataTables\BrandDataTable;
 use App\Http\Controllers\Controller;
+use Str;
 
 class BrandController extends Controller
 {
+    use ImageUploadTrait;
     /**
      * Display a listing of the resource.
      */
@@ -21,7 +25,7 @@ class BrandController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.brand.create');
     }
 
     /**
@@ -29,7 +33,26 @@ class BrandController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'logo' => ['image', 'required', 'max:2000'],
+            'name' => ['required', 'max:200'],
+            'is_featured' => ['required'],
+            'status' => ['required']
+        ]);
+
+        $brand = new Brand();
+
+        $logoPath = $this->uploadImage($request, 'logo', 'uploads');
+        $brand->logo = $request->logo;
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        $brand->is_featured = $request->is_featured;
+        $brand->status = $request->status;
+        $brand->save();
+
+        toastr('Created successfully', 'success');
+        return redirect()->route('admin.brand.index');
+
     }
 
     /**
--- resources/views/admin/brand/index.blade.php
+++ resources/views/admin/brand/index.blade.php
@@ -21,7 +21,7 @@
               <div class="card-header">
                 <h4>All Brands</h4>
                 <div class="card-header-action">
-                    <a href="{{route('admin.category.create')}}" class="btn btn-primary"><i class="fas fa-plus"></i> Create new</a>
+                    <a href="{{route('admin.brand.create')}}" class="btn btn-primary"><i class="fas fa-plus"></i> Create new</a>
                 </div>
               </div>
               <div class="card-body">
