92260124bfc300cf641751df3f69c0683dba37fb
haikaljs <haikaljavascript@gmail.com>
2024-02-19 02:02:57 +0800

    Product - Show created datas at datatable

--- app/DataTables/ProductDataTable.php
+++ app/DataTables/ProductDataTable.php
@@ -22,7 +22,68 @@ class ProductDataTable extends DataTable
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
-            ->addColumn('action', 'product.action')
+            ->addColumn('action', function($query){
+                $editBtn = "<a href='".route('admin.products.edit', $query->id)."' class='btn btn-primary'><i class='far fa-edit'></i></a>";
+
+                $deleteBtn = "<a href='".route('admin.products.destroy', $query->id)."'class='btn btn-danger ml-2 delete-item'><i class='far fa-trash-alt'></i></a>";
+
+                $moreBtn = '<div class="dropdown dropleft d-inline">
+                <button class="btn btn-primary dropdown-toggle ml-1" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-cog"></i>
+                </button>
+                <div class="dropdown-menu">
+                  <a class="dropdown-item has-icon" href="#"><i class="far fa-heart"></i> Action</a>
+                  <a class="dropdown-item has-icon" href="#"><i class="far fa-file"></i> Another action</a>
+                  <a class="dropdown-item has-icon" href="#"><i class="far fa-clock"></i> Something else here</a>
+                </div>
+              </div>';
+
+                return $editBtn.$deleteBtn.$moreBtn;
+            })
+            ->addColumn('image', function($query){
+                return $img = "<img width='100' src='".asset($query->thumb_image)."'></img>";
+            })
+            ->addColumn('type', function($query){
+                switch ($query->product_type) {
+                    case 'new_arrival':
+                        return '<i class="badge badge-success">New Arrival</i>';
+                        break;
+
+                    case 'featured_product':
+                        return '<i class="badge badge-warning">Featured Product</i>';
+                        break;
+
+                    case 'top_product':
+                        return '<i class="badge badge-info">Top Product</i>';
+                            break;
+
+                    case 'best_product':
+                        return '<i class="badge badge-danger">Best Product</i>';
+                            break;
+                    
+                    default:
+                        return '<i class="badge badge-dark">None</i>';
+                        break;
+                }
+            })
+            ->addColumn('status', function($query){
+                if($query->status == 1){
+                    $button = '<label class="custom-switch mt-2">
+                    <input type="checkbox" data-id="'.$query->id.'" checked name="custom-switch-checkbox" class="custom-switch-input change-status">
+                    <span class="custom-switch-indicator"></span>
+                   
+                  </label>';
+                }else{
+                    $button = '<label class="custom-switch mt-2">
+                    <input type="checkbox" data-id="'.$query->id.'" name="custom-switch-checkbox" class="custom-switch-input change-status">
+                    <span class="custom-switch-indicator"></span>
+                   
+                  </label>';
+                }
+            
+                return $button;
+            })
+            ->rawColumns(['image', 'action', 'type', 'status'])
             ->setRowId('id');
     }
 
@@ -63,13 +124,15 @@ class ProductDataTable extends DataTable
     {
         return [
             Column::make('id'),
-            Column::make('add your columns'),
-            Column::make('created_at'),
-            Column::make('updated_at'),
+            Column::make('image'),
+            Column::make('name'),
+            Column::make('price'),
+            Column::make('type')->width(200),
+            Column::make('status'),
             Column::computed('action')
                   ->exportable(false)
                   ->printable(false)
-                  ->width(60)
+                  ->width(200)
                   ->addClass('text-center'),
          
         ];
